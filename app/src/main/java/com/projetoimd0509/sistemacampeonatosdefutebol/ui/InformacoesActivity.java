6f238614e9dbd7a1421b866bbdb1633dcabaf643
Elexandro Torres <elexandro.torres@gmail.com>
2022-01-05 15:25:38 -0300

    Adicionada as três activities que faram parte do aplicativo

---
+++ app/src/main/java/com/projetoimd0509/sistemacampeonatosdefutebol/ui/InformacoesActivity.java
@@ -0,0 +1,16 @@
+package com.projetoimd0509.sistemacampeonatosdefutebol.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.projetoimd0509.sistemacampeonatosdefutebol.R;
+
+public class InformacoesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_informacoes);
+    }
+}
--- app/src/main/java/com/projetoimd0509/sistemacampeonatosdefutebol/ui/MainActivity.java
+++ app/src/main/java/com/projetoimd0509/sistemacampeonatosdefutebol/ui/MainActivity.java
@@ -1,8 +1,10 @@
-package com.projetoimd0509.sistemacampeonatosdefutebol;
+package com.projetoimd0509.sistemacampeonatosdefutebol.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.projetoimd0509.sistemacampeonatosdefutebol.R;
 
 public class MainActivity extends AppCompatActivity {
 
