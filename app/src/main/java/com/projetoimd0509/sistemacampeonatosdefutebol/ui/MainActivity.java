f05f1353a18653cd48d1bfcc8f7327828965ab57
Elexandro Torres <elexandro.torres@gmail.com>
2022-01-05 16:47:32 -0300

    implementação inicial do recycle view da tela principal

--- app/src/main/java/com/projetoimd0509/sistemacampeonatosdefutebol/ui/MainActivity.java
+++ app/src/main/java/com/projetoimd0509/sistemacampeonatosdefutebol/ui/MainActivity.java
@@ -2,15 +2,32 @@ package com.projetoimd0509.sistemacampeonatosdefutebol.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.projetoimd0509.sistemacampeonatosdefutebol.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    FloatingActionButton fabCadastrarCampeonato;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fabCadastrarCampeonato = findViewById(R.id.fab_cadastrar_campeonato);
+        setListeners();
+    }
+
+    void setListeners() {
+        fabCadastrarCampeonato.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CadastroActivity.class);
+            }
+        });
     }
 }
