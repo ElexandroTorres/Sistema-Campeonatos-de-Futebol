bf01c80203e05b3b12f456cbff7e2c163b586ec1
Elexandro Torres <elexandro.torres@gmail.com>
2022-01-07 14:45:46 -0300

    Item de menu na action bar para abrir informações dos integrantes e site do repositorio

--- app/src/main/java/com/projetoimd0509/sistemacampeonatosdefutebol/ui/MainActivity.java
+++ app/src/main/java/com/projetoimd0509/sistemacampeonatosdefutebol/ui/MainActivity.java
@@ -1,11 +1,17 @@
 package com.projetoimd0509.sistemacampeonatosdefutebol.ui;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -15,9 +21,11 @@ import com.projetoimd0509.sistemacampeonatosdefutebol.ui.adapters.ListaCampeonat
 
 public class MainActivity extends AppCompatActivity {
 
-    FloatingActionButton fabCadastrarCampeonato;
-    RecyclerView rvListaCampeonatos;
-    ListaFalsaCampeonatos listaFalsa;
+    private FloatingActionButton fabCadastrarCampeonato;
+    private RecyclerView rvListaCampeonatos;
+    private ListaFalsaCampeonatos listaFalsa;
+
+    private AlertDialog infosDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +37,6 @@ public class MainActivity extends AppCompatActivity {
 
         fabCadastrarCampeonato = findViewById(R.id.fab_cadastrar_campeonato);
         rvListaCampeonatos = findViewById(R.id.rv_lista_campeonatos);
-
 
 
         ListaCampeonatosAdpter adapter = new ListaCampeonatosAdpter(listaFalsa.getLista());
@@ -44,7 +51,23 @@ public class MainActivity extends AppCompatActivity {
         setListeners();
     }
 
-    void setListeners() {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.tela_principal_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_infos:
+                criarDialog();
+                break;
+        }
+        return true;
+    }
+
+    private void setListeners() {
         fabCadastrarCampeonato.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -52,5 +75,29 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+    }
+
+    private void criarDialog() {
+        infosDialog = new AlertDialog.Builder(MainActivity.this)
+                .setTitle("Informações")
+                .setMessage("Integrante 1\nIntegrante 2\nIntegrande 3\nIntegrante 4")
+                .setPositiveButton("Ver Site", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent(Intent.ACTION_VIEW,
+                                Uri.parse("https://github.com/ElexandroTorres/Sistema-Campeonatos-de-Futebol"));
+                        startActivity(intent);
+                    }
+                })
+                .setNegativeButton("Sair", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        infosDialog.dismiss();
+                    }
+                })
+                .create();
+
+        infosDialog.show();
     }
 }
