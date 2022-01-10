9a5246bca3a23a5d3adcca3caed4b0b626bc6095
frankstark <franklinluiz2000@gmail.com>
2022-01-09 21:26:32 -0300

    Codigo alterado

--- app/src/main/java/com/projetoimd0509/sistemacampeonatosdefutebol/ui/CadastroActivity.java
+++ app/src/main/java/com/projetoimd0509/sistemacampeonatosdefutebol/ui/CadastroActivity.java
@@ -5,12 +5,35 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.projetoimd0509.sistemacampeonatosdefutebol.R;
+import com.projetoimd0509.sistemacampeonatosdefutebol.fragmento.CampeonatoDialogFragment;
+import com.projetoimd0509.sistemacampeonatosdefutebol.model.Campeonato;
 
-public class CadastroActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class CadastroActivity extends AppCompatActivity
+        implements CampeonatoDialogFragment.AoSalvarCampeonato {
+
+    List<Campeonato> campeonatos;
+    CampeonatoDialogFragment campeonatoDialogFragment;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+
+        campeonatos = new ArrayList<Campeonato>();
+        campeonatoDialogFragment = CampeonatoDialogFragment.novaInstancia();
+
+    }
+
+    @Override
+    public void salvouCampeonato(Campeonato campeonato) {
+        adicionar(campeonato);
+    }
+
+    private void adicionar(Campeonato campeonato) {
+        campeonatos.add(campeonato);
     }
 }
